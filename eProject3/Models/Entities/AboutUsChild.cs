b98c7ac3abd58e2db80f60423dd8ce0e50d0a82e
Anh Duc <anhduc@gmail.com>
2024-07-18 18:17:14 +0700

    Initial commit

---
+++ eProject3/Models/Entities/AboutUsChild.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eProject3.Models.Entities
+{
+    public class AboutUsChild
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string content { get; set; }
+
+        [ForeignKey("AboutUs")]
+        public int about_id { get; set; }
+        public AboutUs AboutUs { get; set; }
+    }
+}
