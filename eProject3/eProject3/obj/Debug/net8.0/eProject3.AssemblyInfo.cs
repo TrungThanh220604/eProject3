58e2e282477100b572aca9201888cf5fc481a85d
Trung Thanh <trungthanhcva2206@gmail.com>
2024-07-25 20:10:48 +0700

    Update UserAboutUs

--- eProject3/eProject3/obj/Debug/net8.0/eProject3.AssemblyInfo.cs
+++ eProject3/eProject3/obj/Debug/net8.0/eProject3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eProject3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda4ee9c143eed320fc6181d80ea09bac47c9df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beafd11961aefc6dea1f021022dbd2ed1fbc1b3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("eProject3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eProject3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
