5064a7d06ac1e477139fe130007a28f12b31a883
Trung Thanh <trungthanhcva2206@gmail.com>
2024-07-25 20:46:48 +0700

    Merge done

--- eProject3/eProject3/obj/Debug/net8.0/eProject3.AssemblyInfo.cs
+++ eProject3/eProject3/obj/Debug/net8.0/eProject3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eProject3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cf294ddd4a58f1fcd4af58c82416d6ea99fc25c")]
 [assembly: System.Reflection.AssemblyProductAttribute("eProject3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eProject3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
